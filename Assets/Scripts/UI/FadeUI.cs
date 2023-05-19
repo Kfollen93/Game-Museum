907284a2286a05d147fff80b2c4700aa24647e79
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2023-05-18 21:00:04 -0700

    Clean up script

--- Assets/Scripts/UI/FadeUI.cs
+++ Assets/Scripts/UI/FadeUI.cs
@@ -7,25 +7,10 @@ public class FadeUI : MonoBehaviour
     [SerializeField] private Image _backgroundImage;
     [SerializeField] private TMP_Text _textMeshPro;
     [SerializeField] private float _duration = 1f;
-
+    [SerializeField] private Button _welcomeButton;
+    [SerializeField] private TMP_Text _welcomeText;
     private float timer = 0f;
     private bool isFadingOut = false;
-
-    void Start()
-    {
-        if (_backgroundImage != null)
-        {
-            Color imageColor = _backgroundImage.color;
-            imageColor.a = 1f;
-            _backgroundImage.color = imageColor;
-        }
-        if (_textMeshPro != null)
-        {
-            Color textColor = _textMeshPro.color;
-            textColor.a = 1f;
-            _textMeshPro.color = textColor;
-        }
-    }
 
     void Update()
     {
@@ -38,24 +23,57 @@ public class FadeUI : MonoBehaviour
         {
             timer += Time.deltaTime;
             float alpha = Mathf.Lerp(1f, 0f, timer / _duration);
-            if (_backgroundImage != null)
-            {
-                Color imageColor = _backgroundImage.color;
-                imageColor.a = alpha;
-                _backgroundImage.color = imageColor;
-            }
-            if (_textMeshPro != null)
-            {
-                Color textColor = _textMeshPro.color;
-                textColor.a = alpha;
-                _textMeshPro.color = textColor;
-            }
+
+            FadeBlackScreenBackgroundAlpha(alpha);
+            FadeTextOnBackgroundAlpha(alpha);
+            FadeWelcomeButtonTextAlpha(alpha);
+            FadeWelcomeButton(alpha);
+
             if (timer >= _duration)
             {
                 isFadingOut = false;
                 timer = 0f;
                 gameObject.SetActive(false);
             }
+        }
+    }
+
+    private void FadeWelcomeButton(float alpha)
+    {
+        if (_welcomeButton != null)
+        {
+            Color buttonColor = _welcomeButton.colors.normalColor;
+            buttonColor.a = alpha;
+        }
+    }
+
+    private void FadeWelcomeButtonTextAlpha(float alpha)
+    {
+        if (_welcomeText != null)
+        {
+            Color textColor = _welcomeText.color;
+            textColor.a = alpha;
+            _welcomeText.color = textColor;
+        }
+    }
+
+    private void FadeTextOnBackgroundAlpha(float alpha)
+    {
+        if (_textMeshPro != null)
+        {
+            Color textColor = _textMeshPro.color;
+            textColor.a = alpha;
+            _textMeshPro.color = textColor;
+        }
+    }
+
+    private void FadeBlackScreenBackgroundAlpha(float alpha)
+    {
+        if (_backgroundImage != null)
+        {
+            Color imageColor = _backgroundImage.color;
+            imageColor.a = alpha;
+            _backgroundImage.color = imageColor;
         }
     }
 
